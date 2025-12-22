13340a1ccd2457c7ccbce3966161318c86ad94cd
Ilyasin <ilyasin7700@gmail.com>
2025-12-22 09:55:37 +0700

    add file object common

---
+++ src/Common/FileObject.cs
@@ -0,0 +1,15 @@
+﻿namespace NDB.Abstraction.Common;
+
+public sealed class FileObject
+{
+    public string Filename { get; init; } = string.Empty;
+    public string MimeType { get; init; } = string.Empty;
+    public string Base64 { get; init; } = string.Empty;
+}
+
+public class FileByteObject
+{
+    public byte[]? File { get; set; }
+    public string Filename { get; init; } = string.Empty;
+    public string MimeType { get; init; } = string.Empty;
+}
